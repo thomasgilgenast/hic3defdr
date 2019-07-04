attempting to fix interplay between simulations and conditional scaling factors
also removed pip cache from pipelines because it appears to be mostly
redundant with the venv cache

fast3defdr/analysis.py
@@ -23,6 +23,7 @@ from fast3defdr.plotting.histograms import plot_pvalue_histogram
 from fast3defdr.plotting.dispersion import plot_mvr
 from fast3defdr.plotting.ma import plot_ma
 from fast3defdr.plotting.grid import plot_grid
+from fast3defdr.progress import tqdm_maybe as tqdm
 
 
 class Fast3DeFDR(object):
@@ -635,9 +636,23 @@ class Fast3DeFDR(object):
                 index=repnames
             ).to_csv(design_file)
 
+        # rewrite size_factor matrix in terms of distance
+        if len(size_factors.shape) == 2:
+            eprint('  converting size factors')
+            dist = col - row
+            n_dists = dist.max() + 1
+            new_size_factors = np.zeros((n_dists, size_factors.shape[1]))
+            for d in tqdm(range(n_dists)):
+                idx = np.argmax(dist == d)
+                new_size_factors[d, :] = size_factors[idx, :]
+            size_factors = new_size_factors
+
         # scramble bias and size_factors
         bias = bias[:, (np.arange(n_sim)+1) % n_sim]
-        size_factors = size_factors[(np.arange(n_sim)+3) % n_sim]
+        if len(size_factors.shape) == 1:
+            size_factors = size_factors[(np.arange(n_sim)+3) % n_sim]
+        else:
+            size_factors = size_factors[:, (np.arange(n_sim)+3) % n_sim]
 
         # simulate and save
         classes, sim_iter = simulate(

fast3defdr/simulation.py
@@ -79,7 +79,9 @@ def simulate(row, col, mean, disp_fn, bias, size_factors, clusters, beta=0.5,
         that replicate.
     size_factors : np.ndarray
         Vector of size factors to use for simulating for each to-be-simulated
-        replicate.
+        replicate. To use a different size factor at different distance scales,
+        pass a matrix whose rows correspond to distance scales and whose columns
+        correspond to replicates.
     clusters : list of list of tuple
         The outer list is a list of clusters which represent the locations of
         loops. Each cluster is a list of (i, j) tuples marking the position of
@@ -145,7 +147,7 @@ def simulate(row, col, mean, disp_fn, bias, size_factors, clusters, beta=0.5,
     classes[(classes == 'up B') | (classes == 'down A')] = 'B'
 
     eprint('  preparing generator')
-    n_sim = len(size_factors)
+    n_sim = size_factors.shape[-1]
     mean_a = mean_a_csr[new_row, new_col].A1
     mean_b = mean_b_csr[new_row, new_col].A1
     assert np.all(mean_a >= 0)
@@ -155,7 +157,11 @@ def simulate(row, col, mean, disp_fn, bias, size_factors, clusters, beta=0.5,
         for j, m in zip(range(n_sim), [mean_a]*(n_sim/2) + [mean_b]*(n_sim/2)):
             eprint('  biasing and simulating rep %i/%i' % (j+1, n_sim))
             # bias mean
-            bm = m*bias[new_row, j]*bias[new_col, j]*size_factors[j] + 0.1
+            if len(size_factors.shape) == 1:
+                bm = m*bias[new_row, j]*bias[new_col, j]*size_factors[j] + 0.1
+            else:
+                bm = m*bias[new_row, j] * bias[new_col, j] * \
+                    size_factors[new_col-new_row, j] + 0.1
             assert np.all(bm > 0)
 
             # establish cov