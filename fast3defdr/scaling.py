5111b1e5861dce52be6bb03fa901c18bc0c8451b
Thomas Gilgenast <thomasgilgenast@gmail.com>
2019-07-16 13:48:02 -0400

    makes filter_zeros=True a default kwarg on median_of_ratios
    previously filtering zeros was being done via the idx kwarg of
    conditional()
     - we have removed this kwarg
     - by performing the filtering using this kwarg we excluded the
       possibility of using median_of_ratios scaling without the conditional
       part (this now works as expected)
     - this reflects a design decision where median of ratios is just not
       expected to work without the zero filtering, so we push it down to
       the lowest level and make it the default behavior

--- fast3defdr/scaling.py
+++ fast3defdr/scaling.py
@@ -6,7 +6,7 @@ from fast3defdr.binning import equal_bin
 from fast3defdr.progress import tqdm_maybe as tqdm
 
 
-def median_of_ratios(data):
+def median_of_ratios(data, filter_zeros=True):
     """
     Computes size factors for a dataset using the median of ratios method.
 
@@ -14,13 +14,19 @@ def median_of_ratios(data):
     ----------
     data : np.ndarray
         Rows correspond to pixels, columns correspond to replicates.
+    filter_zeros : bool
+        Pass True to filter out pixels with a zero value in any replicate. Pass
+        False to include all data.
 
     Returns
     -------
     np.ndarray
         Vector of size factors, per replicate.
     """
-    return np.median(data / gmean(data, axis=1)[:, None], axis=0)
+    idx = np.all(data > 0, axis=1) if filter_zeros \
+        else np.ones(data.shape[0], dtype=bool)
+    return np.median(data[idx, :] /
+                     gmean(data[idx, :], axis=1)[:, None], axis=0)
 
 
 def simple_scaling(data):
@@ -41,11 +47,10 @@ def simple_scaling(data):
     return s / gmean(s)
 
 
-def conditional(data, dist, fn, idx=None, n_bins=None):
+def conditional(data, dist, fn, n_bins=None):
     """
     Applies a size factor computing function ``fn`` to ``data`` conditioning on
-    ``dist``, optionally applying ``idx`` to filter inputs to ``fn`` and/or
-    binning ``dist`` into ``n_bins`` equal-number bins.
+    ``dist``, optionally binning ``dist`` into ``n_bins`` equal-number bins.
 
     Parameters
     ----------
@@ -55,14 +60,9 @@ def conditional(data, dist, fn, idx=None, n_bins=None):
         The distance of each pixel in ``data``.
     fn : function
         A function that computes a size factor given some data.
-    idx : np.ndarray, optional
-        A boolean vector indicating whether or not each pixel in ``data`` should
-        be included in what's passed to ``fn``.
     n_bins : int, optional
         Pass an int to bin distance into this many equal-number bins.
     """
-    if idx is None:
-        idx = np.ones_like(dist, dtype=bool)
     result = np.zeros_like(data, dtype=float)
     if n_bins:
         bins = equal_bin(dist, n_bins)
@@ -70,7 +70,7 @@ def conditional(data, dist, fn, idx=None, n_bins=None):
         bins = dist
     for b in tqdm(np.unique(bins)):
         dist_idx = bins == b
-        result[dist_idx, :] = fn(data[dist_idx & idx, :])
+        result[dist_idx, :] = fn(data[dist_idx, :])
     return result
 
 
@@ -93,9 +93,7 @@ def conditional_mor(data, dist, n_bins=None):
     np.ndarray
         Matrix of size factors, per pixel (rows) and per replicate (columns).
     """
-    zero_idx = np.all(data > 0, axis=1)
-    return conditional(data, dist, median_of_ratios, idx=zero_idx,
-                       n_bins=n_bins)
+    return conditional(data, dist, median_of_ratios, n_bins=n_bins)
 
 
 def conditional_scaling(data, dist, n_bins=None):
