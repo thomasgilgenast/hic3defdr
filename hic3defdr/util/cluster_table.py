95b57b5e5394ca275afe96fac5548093edba7dba
Thomas Gilgenast <thomasgilgenast@gmail.com>
2020-02-12 20:58:23 -0500

    preliminary work on #32
    this commit adds the function
    `hic3defdr.util.cluster_table.add_column_to_cluster_table()` which we
    intend to use to add additional columns to the cluster tables

--- hic3defdr/util/cluster_table.py
+++ hic3defdr/util/cluster_table.py
@@ -1,3 +1,5 @@
+import numpy as np
+import scipy.sparse as sparse
 import pandas as pd
 
 from lib5c.util.bed import parse_feature_from_string
@@ -185,3 +187,146 @@ def load_cluster_table(table_filename):
     df = pd.read_csv(table_filename, sep='\t', index_col=0)
     df['cluster'] = df['cluster'].apply(cluster_from_string)
     return df
+
+
+def add_columns_to_cluster_table(cluster_table, name_pattern, row, col,
+                                 data, labels=None, reducer='mean', chrom=None):
+    r"""
+    Adds new data columns to an existing cluster table by evaluating a sparse
+    dataset specified by ``row``, ``col``, ``data`` at the pixels in each
+    cluster and combining the resulting values using ``reducer``.
+
+    This function operates in-place.
+
+    Parameters
+    ----------
+    cluster_table : pd.DataFrame
+        Must contain a "cluster" column. If the values in this column are
+        strings, they will be "corrected" to list of lists of int in-place.
+    name_pattern : str
+        The name of the column to fill in. If ``data`` contains more than one
+        column, multiple columns will be added - include exactly one ``%s`` in
+        the ``name_pattern``, then the ``i`` th new column will be called
+        ``name_pattern % labels[i]``.
+    row, col, data : np.ndarray
+        Sparse format data to use to determine the value to fill in for each
+        cluster for each new column. ``row`` and ``col`` must be parallel to the
+        first dimension of ``data``. If ``data`` is two-dimensional, you must
+        pass ``labels`` to label the columns and include a ``%s`` in
+        ``name_pattern``.
+    labels : list of str, optional
+        If ``data`` is two-dimensional, pass a list of strings labeling the
+        columns of ``data``.
+    reducer : {'mean', 'max', 'min'}
+        The function to use to combine the values for the pixels in each
+        cluster.
+    chrom : str, optional
+        If the ``cluster_table`` contains data from multiple chromosomes, pass
+        the name of the chromosome that ``row, col, data`` correspond to and
+        only clusters for that chromosome will have their new column
+        created/updated. If the ``cluster_table`` contains data from only one
+        chromosome, pass None to update all clusters in the ``cluster_table``.
+
+    Examples
+    --------
+    >>> import numpy as np
+    >>> from hic3defdr.util.cluster_table import clusters_to_table, \
+    ...     add_columns_to_cluster_table
+    >>> # basic test: clusters all on one chromosome
+    >>> clusters = [[(1, 2), (1, 1)], [(4, 4),  (3, 4)]]
+    >>> res = 10000
+    >>> df = clusters_to_table(clusters, 'chrX', res)
+    >>> row, col = zip(*sum(clusters, []))
+    >>> data = np.array([[1, 2],
+    ...                  [3, 4],
+    ...                  [5, 6],
+    ...                  [7, 8]], dtype=float)
+    >>> add_columns_to_cluster_table(df, '%s_mean', row, col, data,
+    ...                              labels=['rep1', 'rep2'])
+    >>> df.iloc[0, :]
+    us_chrom                    chrX
+    us_start                   10000
+    us_end                     20000
+    ds_chrom                    chrX
+    ds_start                   10000
+    ds_end                     30000
+    cluster_size                   2
+    cluster         [[1, 2], [1, 1]]
+    rep1_mean                      2
+    rep2_mean                      3
+    Name: chrX:10000-20000_chrX:10000-30000, dtype: object
+    >>> # advanced test: two chromosomes
+    >>> df1 = clusters_to_table(clusters, 'chr1', res)
+    >>> df2 = clusters_to_table(clusters, 'chr2', res)
+    >>> df = pd.concat([df1, df2], axis=0)
+    >>> # add chr1 info
+    >>> add_columns_to_cluster_table(df, '%s_mean', row, col, data,
+    ...                              labels=['rep1', 'rep2'], chrom='chr1')
+    >>> # chr1 cluster has data filled in
+    >>> df.ix[0, ['rep1_mean', 'rep2_mean']]
+    rep1_mean    2
+    rep2_mean    3
+    Name: chr1:10000-20000_chr1:10000-30000, dtype: object
+    >>> # chr2 cluster has nans
+    >>> df.ix[2, ['rep1_mean', 'rep2_mean']]
+    rep1_mean    NaN
+    rep2_mean    NaN
+    Name: chr2:10000-20000_chr2:10000-30000, dtype: object
+    >>> # add chr2 info, with different data (reversed row order)
+    >>> add_columns_to_cluster_table(df, '%s_mean', row, col, data[::-1, :],
+    ...                              labels=['rep1', 'rep2'], chrom='chr2')
+    >>> # now the chr2 clusters have data
+    >>> df.ix[2, ['rep1_mean', 'rep2_mean']]
+    rep1_mean    6
+    rep2_mean    7
+    Name: chr2:10000-20000_chr2:10000-30000, dtype: object
+    >>> # edge case: data is a vector
+    >>> df = clusters_to_table(clusters, 'chrX', res)
+    >>> add_columns_to_cluster_table(df, 'value', row, col, data[:, 0])
+    >>> df.iloc[0, :]
+    us_chrom                    chrX
+    us_start                   10000
+    us_end                     20000
+    ds_chrom                    chrX
+    ds_start                   10000
+    ds_end                     30000
+    cluster_size                   2
+    cluster         [[1, 2], [1, 1]]
+    value                          2
+    Name: chrX:10000-20000_chrX:10000-30000, dtype: object
+    """
+    # determine reducer
+    reducer = {
+        'mean': np.mean,
+        'max': np.max,
+        'min': np.min
+    }[reducer]
+
+    # determine which rows of the cluster table lie on this chromosome
+    row_idx = np.ones(len(cluster_table), dtype=bool)
+    if chrom is not None:
+        row_idx = (cluster_table['us_chrom'] == chrom) & \
+                  (cluster_table['ds_chrom'] == chrom)
+
+    # promote data to 2D
+    if data.ndim == 1:
+        data = data[:, None]
+
+    # create CSRs for each column of data
+    csrs = [sparse.coo_matrix((data[:, i], (row, col))).tocsr()
+            for i in range(data.shape[1])]
+
+    # iterate over columns of data
+    for i in range(data.shape[1]):
+        # determine the name of the new column
+        column_name = name_pattern % labels[i] if labels is not None \
+            else name_pattern
+
+        # add the new column if it doesn't exist yet
+        if column_name not in cluster_table.columns:
+            cluster_table[column_name] = np.ones(len(cluster_table)) * np.nan
+
+        # fill in information for the new column
+        cluster_table.loc[row_idx, column_name] = \
+            cluster_table.loc[row_idx, 'cluster']\
+            .map(lambda x: reducer(csrs[i][tuple(zip(*x))]))
